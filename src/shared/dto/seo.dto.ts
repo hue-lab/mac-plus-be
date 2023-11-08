7328840a116a57b9177a56de2eeef4922abaaac8
AndreiYa <newminer@bk.ru>
2023-11-08 19:36:29 +0300

    MACPLUS-68 | alt for images

--- src/shared/dto/seo.dto.ts
+++ src/shared/dto/seo.dto.ts
@@ -1,7 +1,13 @@
+class SeoImageAlt {
+  imageName: string;
+  imageAlt: string;
+}
+
 export class SeoDTO {
   seoTitle?: string;
   seoDescription?: string;
   seoKeywords?: string[];
   seoAuthor?: string;
   seoUrl?: string;
+  seoImageAlt?: SeoImageAlt;
 }
