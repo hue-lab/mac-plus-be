908f54f345b8190a915c3d3f98f8803b20a28f09
zidiks <zidiks228@gmail.com>
2024-04-08 09:15:01 +0300

    upd: email to phone

--- src/notify/notify.service.ts
+++ src/notify/notify.service.ts
@@ -45,7 +45,7 @@ export class NotifyService {
     const message =
       `Новый запрос\n` +
       `Имя: ${notifyMessageDTO.name}\n` +
-      `Email: ${notifyMessageDTO.email}\n` +
+      `Телефон: ${notifyMessageDTO.email}\n` +
       `Комментарий:\n` +
       `${notifyMessageDTO.message}`;
     await this.bot.telegram.sendMessage(chartId, message);
